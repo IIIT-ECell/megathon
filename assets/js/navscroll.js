b73a1a391339c62b50f5b0c28286ca75ceb26fa7
zubairabid <zubairabid1999+github@gmail.com>
2018-06-15 10:01:20 +0530

    Fixed Sponsors, landing. disabled fade

--- assets/js/navscroll.js
+++ assets/js/navscroll.js
@@ -14,7 +14,7 @@ let shiftY = (cov.height())/2 - nav.height();
 // To make sure the JS is optimal, i.e. There's no performance issue
 // Accomplished by making the scrollThing wait
 let scrollHandler = {
-  allow: true,
+  allow: false,
   reallow: function() {
     scrollHandler.allow = true;
   },
